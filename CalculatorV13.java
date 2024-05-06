514dd1727d430fe99e8d639f589f9497b46a9e0c
NiHroM88 <157886226+NiHroM88@users.noreply.github.com>
2024-05-06 23:30:00 +0500

    Update CalculatorV13.java
    Внес кучу римских цифр

--- CalculatorV13.java
+++ CalculatorV13.java
@@ -71,7 +71,10 @@ public class CalculatorV13 {
 
 }
 class Roman {
-    static String[] romanArray = new String[]{"0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"}; // массив на римскеие цифры
+    static String[] romanArray = new String[]{"0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII",
+            "XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXIV","XXV","XXVII","XXVIII","XXX","XXXII","XXXV", "XL",
+            "XLII","XLV","XLVIII","XLIX","L","LIV","LVI","LX","LXIII","LXIV","LXX","LXXII",
+            "LXXX","LXXXI","XC","C"}; // массив на римскеие цифры
 
     public static boolean isRoman(String val) {
         for (int i=0; i<romanArray.length; i++) {
